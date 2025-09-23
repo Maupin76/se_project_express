fa4f31ace896f0dec9f6326c093684ec2cf197a5
Douglas Maupin <douglasmaupin@yahoo.com>
2025-09-23 16:00:38 -0600

    Ensure /users routes are protected by auth, fix: added auth middleware and 401; protect /users

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,20 @@
 const router = require("express").Router();
 
 const { createUser, login } = require("../controllers/users");
+const { getItems } = require("../controllers/clothingItems");
+const auth = require("../middlewares/auth");
+
 const userRouter = require("./users");
 const itemRouter = require("./items");
 
-// Public auth endpoints
+// Public endpoints
 router.post("/signin", login);
 router.post("/signup", createUser);
+router.get("/items", getItems);
 
-// Other routers (will be protected in Step 5)
+// Everything below this line requires a valid JWT
+router.use(auth);
+
 router.use("/users", userRouter);
 router.use("/items", itemRouter);
 
